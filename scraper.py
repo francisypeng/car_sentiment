52815151af6fa49d3dc0f4cdc9977099d048b8eb
francisypeng <fypeng@emory.edu>
2022-10-19 12:34:17 -0400

    setup scraper.py and updates to scraper

--- scraper.py
+++ scraper.py
@@ -2,7 +2,71 @@ import pandas as pd
 import numpy as np
 import xmltodict
 import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 page = requests.get("https://carsandbids.com/auctions/3qbOzbzy/2004-porsche-911-gt3")
 page.status_code
+
+def load_and_scroll(driver):
+    """
+    Scrolls and loads more comments until there are no more comments to load.
+    This ensures that the full auction page is obtained before doing any further scraping.
+    """
+    # Scroll to bottom of page to dynamically load everything
+    lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    match=False
+    while(match==False):
+        lastCount = lenOfPage
+        time.sleep(3)
+        lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        if lastCount==lenOfPage:
+            match=True
+    time.sleep(3)
+
+    # find load more comments button and click
+    eop = False
+    while(eop == False):
+        try:
+            load_more = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "load-more"))
+            )
+        except:
+            # if no more to load, stop scrolling
+            print("end of page reached")
+            eop = True
+        else:
+            # if more to load, click and scroll
+            load_more.click()
+            lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+            match=False
+            while(match==False):
+                lastCount = lenOfPage
+                time.sleep(3)
+                lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+                if lastCount==lenOfPage:
+                    match=True
+            time.sleep(3)
+    return None
+
+def get_comments(driver):
+
+    return None
+
+
+def main():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) # create driver
+    driver.get("https://carsandbids.com/auctions/rxV7kL4j/2002-porsche-911-carrera-coupe") # get webpage
+    load_and_scroll(driver)
+
+
+
+if __name__ == '__main__':
+    main()
