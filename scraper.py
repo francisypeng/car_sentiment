8bf6a3cf2444486f14828f882240d4d4547174c4
francisypeng <fypeng@emory.edu>
2022-11-05 00:30:06 -0400

    test commit 3

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException  
 import time
 
-#test commit 2
+#test commit 3
 
 def load_and_scroll(driver):
     """
